2e22537efcb3f9839f064a3711d990644c507c58
Dimenzio <dimenzioderechte@gmail.com>
2020-10-17 22:09:54 +0200

    Added Twitter image grabbing

--- TwitterTierlist/tierlist.js
+++ TwitterTierlist/tierlist.js
@@ -1,5 +1,11 @@
-document.getElementById("inputButton").addEventListener("click", inputName);
+document.addEventListener("DOMContentLoaded", function(event)  {
+    document.getElementById("inputButton").addEventListener("click", inputName);
+})
 
 function inputName() {
-    alert("You clicked the button!");
+    let input = prompt("Enter the name of the account you search!");
+    let img = document.createElement("img");
+    img.src = "https://unavatar.now.sh/twitter/" + input;
+    let div = document.getElementById("imgDiv");
+    div.appendChild(img);
 }
